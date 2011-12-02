405285aa0f0f45da9f9551b8fe33fbfc14608ba4
Steven Koch <stvkoch@gmail.com>
2011-12-02 12:31:54 +0000

    format code

--- lib/carrierwaves_uploader_callbacks.rb
+++ lib/carrierwaves_uploader_callbacks.rb
@@ -1,16 +1,15 @@
 # encoding: utf-8
-
 module CarrierWave
   module Uploader
     module Callbacks
       extend ActiveSupport::Concern
 
       def with_callbacks(kind, *args)
-				begin
-        	self.class._before_callbacks[kind].each { |c| send c, *args }
-        	yield
-				rescue CarrierWave::Uploader::Exceptions::Base
-				end
+        begin
+          self.class._before_callbacks[kind].each { |c| send c, *args }
+          yield
+        rescue CarrierWave::Uploader::Exceptions::Base
+        end
         self.class._after_callbacks[kind].each { |c| send c, *args }
       end
 
